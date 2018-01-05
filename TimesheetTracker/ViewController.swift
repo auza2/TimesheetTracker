45cc5286f23df974ac0f2a585a913265068303bc
Jamie Auza <jamieauza@Jamies-MacBook-Pro.local>
2018-01-05 13:29:57 -0600

    Initial Commit

---
+++ TimesheetTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimesheetTracker
+//
+//  Created by Jamie Auza on 1/5/18.
+//  Copyright © 2018 Jamie Auza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
